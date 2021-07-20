e3977caf5c220c526646d7c738897b3c4e8e886d
LinhNguyen2k <55703510+anhlinh27092000@users.noreply.github.com>
2021-07-20 23:16:47 +0700

    Update layout Admin

--- MyPham/MyPham/Areas/Admin/Controllers/HomeController.cs
+++ MyPham/MyPham/Areas/Admin/Controllers/HomeController.cs
@@ -13,6 +13,60 @@ namespace MyPham.Areas.Admin.Controllers
         {
             return View();
         }
-
+        public ActionResult QuanLySanPham()
+        {
+            return View();
+        }
+        public ActionResult QuanLyDanhMuc()
+        {
+            return View();
+        } public ActionResult QuanLyHoaDon()
+        {
+            return View();
+        }   
+        public ActionResult QuanLyTaiKhoan()
+        {
+            return View();
+        }  
+        public ActionResult QuanLyPhanQuyen()
+        {
+            return View();
+        }
+        public ActionResult CapNhatSanPham()
+        {
+            return View();
+        }       
+        public ActionResult ThemSanPham()
+        {
+            return View();
+        }
+        public ActionResult ChiTietSanPham()
+        {
+            return View();
+        }
+        public ActionResult ThemDanhMuc()
+        {
+            return View();
+        }
+        public ActionResult CapNhatDanhMuc()
+        {
+            return View();
+        }
+        public ActionResult ThemTaiKhoan()
+        {
+            return View();
+        }
+        public ActionResult CapNhatTaiKhoan()
+        {
+            return View();
+        }
+        public ActionResult ThemPhanQuyen()
+        {
+            return View();
+        }
+        public ActionResult CauHinhQuyen()
+        {
+            return View();
+        }
     }
 }
