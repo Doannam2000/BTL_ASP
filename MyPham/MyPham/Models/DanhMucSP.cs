7be45a8a91851ab203fe968d95b633bc949fd51c
khucvandoan <doankhuc123@gmail.com>
2021-07-30 17:37:59 +0700

    update model danh muc

--- MyPham/MyPham/Models/DanhMucSP.cs
+++ MyPham/MyPham/Models/DanhMucSP.cs
@@ -1,7 +1,8 @@
-namespace MyPham.Models
+﻿namespace MyPham.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -15,11 +16,13 @@ namespace MyPham.Models
             SanPham = new HashSet<SanPham>();
         }
 
+        [DisplayName("Mã danh mục")]
         [Key]
         public int MaDM { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Tên danh mục không được để trống!")]
         [StringLength(80)]
+        [DisplayName("Tên danh mục")]
         public string TenDM { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
