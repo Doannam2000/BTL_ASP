f9a803b0df168800ba581885bcba3d340c2cc234
DoanNam2000 <doanduynam2000@gmail.com>
2021-08-06 21:52:38 +0700

    update check tk , index admin

--- MyPham/MyPham/Areas/Admin/Controllers/HomeController.cs
+++ MyPham/MyPham/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyPham.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,31 @@ namespace MyPham.Areas.Admin.Controllers
 {
     public class HomeController : BaseController
     {
+        private MyPhamDB db = new MyPhamDB();
         // GET: Admin/Home
         public ActionResult Index()
         {
+            var doanhThu = db.Chi_Tiet_Gio_Hang.ToList();
+            var hoadon = db.HoaDon.ToList();
+            var taikhoan = db.TaiKhoan.Where(s => s.MaQuyen == 3).ToList();
+            var sanpham = db.SanPham.ToList();
+
+            double doanht = 0;
+            int slton = 0;
+            int slDaBan = 0;
+            foreach (var item in doanhThu)
+            {
+                slDaBan += item.SoLuongMua;
+                doanht += item.SoLuongMua * Convert.ToDouble(item.GiaSP);
+            }
+            foreach (var item in sanpham)
+            {
+                slton += item.SoLuongTon;
+            }
+            ViewBag.DoanhThu = doanht;
+            ViewBag.TongSP = (slton + slDaBan);
+            ViewBag.SLBan = slDaBan;
+            ViewBag.SLKhach = taikhoan.Count;
             return View();
         }
        public ActionResult QuanLyPhanQuyen()
